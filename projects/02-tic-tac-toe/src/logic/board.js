420356bc8db140b46dff9f48e3a8da9dc5fbb57a
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-18 19:23:38 -0600

    refactor(02-tic-tac-toe): :art: Separete the logic and the constants of the game

---
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,22 @@
+import { WINNERS_COMBOS } from "../constants";
+
+
+// Check if teh game is ended
+export const checkGameEnded = (boardToCheck) => boardToCheck.every(square => square !== null)
+
+// Check if there is a winner
+export const checkWinner = (boardToCheck) => {
+  for(const combo of WINNERS_COMBOS) {
+    const [a, b, c] = combo;
+
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      const newWinner = boardToCheck[a];
+      return newWinner;
+    }
+  }
+  return null;
+}
