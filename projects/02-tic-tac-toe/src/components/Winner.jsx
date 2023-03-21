23844c821de4c905f6a537233948ee9358d8a06a
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-20 20:33:55 -0600

    chore(02-tic-tac-toe): :rotating_light: Appply fixes by standtard linter

--- projects/02-tic-tac-toe/src/components/Winner.jsx
+++ projects/02-tic-tac-toe/src/components/Winner.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
-import { Square } from './Square';
+import { Square } from './Square'
 
-export function Winner({winner, resetGame}) {
-  if(winner === null) return;
+export function Winner ({ winner, resetGame }) {
+  if (winner === null) return
   return (
-        <section className='winner'>
-          <div className='text'>
-            <h2>
-              {
+    <section className='winner'>
+      <div className='text'>
+        <h2>
+          {
                 winner === false
                   ? 'Empate'
                   : 'Gano:'
               }
-            </h2>
-          </div>
-          <header className="win">
-            {winner && <Square>{winner}</Square>}
-          </header>
+        </h2>
+      </div>
+      <header className='win'>
+        {winner && <Square>{winner}</Square>}
+      </header>
 
-          <footer>
-            <button onClick={resetGame}>Start again</button>
-          </footer>
-        </section>
+      <footer>
+        <button onClick={resetGame}>Start again</button>
+      </footer>
+    </section>
   )
 }
