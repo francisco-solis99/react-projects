f2588b692d57d2a61f54d178f0af0c1d81585751
francisco-solis99 <franciscosoliscss@gmail.com>
2023-04-12 21:43:04 -0600

    refactor(05-searcher-movies): :recycle: Refactor the search effect as custom hook

--- projects/05-searcher-movie/src/App.jsx
+++ projects/05-searcher-movie/src/App.jsx
@@ -3,19 +3,38 @@ import './App.css'
 
 // Components
 import { Movies } from './components/Movies'
+
+// Custom Hooks
+import { useSearch } from './hooks/useSearch'
 import { useMovies } from './hooks/useMovies'
 
 function App () {
   const { movies } = useMovies()
+  const { query, setQuery, error } = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChangeQuery = (event) => {
+    const newQuery = event.target.value
+    setQuery(newQuery)
+  }
+
+  // Use the form in a controlled way (by the state) perhaps you can use the uncontrolled way which is in most of the cases better
 
   return (
     <div className='wrapper'>
       <header>
         <h1>Searcher Movies 🍿</h1>
-        <form>
-          <input type='text' placeholder='Avengers, Star Wars, The matrix' />
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={handleChangeQuery} value={query} placeholder='Avengers, Star Wars, The matrix' />
           <button type='submit'>Search</button>
         </form>
+        {
+          error && <p style={{ color: 'red' }}>{error}</p>
+        }
       </header>
 
       <main>
---
+++ projects/05-searcher-movie/src/hooks/useSearch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useSearch () {
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstTimeInput = useRef(true)
+
+  useEffect(() => {
+    console.log(isFirstTimeInput.current)
+    if (isFirstTimeInput.current) {
+      isFirstTimeInput.current = query === ''
+      return
+    }
+
+    if (query === '') {
+      setError('There is not a movie with blank space')
+      return
+    }
+
+    if (query.length < 2) {
+      setError('The movie to search must have at least more tha 1 character')
+      return
+    }
+
+    setError(null)
+  }, [query])
+
+  return { query, setQuery, error }
+}
