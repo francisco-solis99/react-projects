5cbc0e1cc016ac83b93fc1535b2250e0c643f80d
francisco-solis99 <franciscosoliscss@gmail.com>
2023-04-13 00:24:34 -0600

    feat(05-searcher-movies): :art: Avoid the search both times the same query search using a useRef

--- projects/05-searcher-movie/src/hooks/useMovies.js
+++ projects/05-searcher-movie/src/hooks/useMovies.js
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { searchMovies } from '../services/movies'
 
-export function useMovies () {
+export function useMovies ({ query }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
+  const previousQuery = useRef(query)
 
-  const getMovies = async ({ query }) => {
+  const getMovies = async () => {
+    // void to search both or more times the same query search
+    if (query === previousQuery.current) return
     try {
       setLoading(true)
+      // Save the ref (the useRef does not chane between renders and does no make the component render again)
+      previousQuery.current = query
       const movies = await searchMovies({ query })
       setMovies(movies)
     } catch (e) {
