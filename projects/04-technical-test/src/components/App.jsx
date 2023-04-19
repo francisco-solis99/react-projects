e954899fa46b8b09632a6804c4cbdb44c94b657b
francisco-solis99 <franciscosoliscss@gmail.com>
2023-04-18 21:52:04 -0600

    chore(04-Test-Technique): :art: Use the custom hooks made in the main appilcation

--- projects/04-technical-test/src/components/App.jsx
+++ projects/04-technical-test/src/components/App.jsx
@@ -1,47 +1,19 @@
 /* eslint-disable react/jsx-closing-tag-location */
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-const API_FACTS = 'https://catfact.ninja/fact'
-const API_CATS = 'https://cataas.com/cat/says'
+// Custom hooks
+import { useCatImageUrl } from '../hooks/useCatImageUrl'
+import { useCatFact } from '../hooks/useCatFact'
 
 export default function App () {
-  const [fact, setFact] = useState(null)
-  const [catImageSrc, setCatImageSrc] = useState(null)
-  const [isChangeFact, isSetChangeFact] = useState(true)
+  const { fact, getCatFact } = useCatFact()
+  const { catImageUrl } = useCatImageUrl({ fact })
 
-  useEffect(() => {
-    const getRandomFact = async () => {
-      try {
-        const response = await fetch(API_FACTS)
-        const { fact: factText } = await response.json()
-        setFact(factText)
-      } catch (error) {
-        console.log(error)
-      }
-    }
+  const handleClick = async () => {
+    getCatFact()
+  }
 
-    if (isChangeFact) getRandomFact()
-  }, [isChangeFact])
-
-  useEffect(() => {
-    if (!fact) return
-    const getCatImage = async () => {
-        try {
-          const firstWordFact = fact.split(' ')[0]
-          const response = await fetch(`${API_CATS}/${firstWordFact}`)
-          const { url } = response
-          setCatImageSrc(url)
-        } catch (error) {
-          console.log(error)
-        }
-    }
-    getCatImage()
-    setTimeout(() => {
-      isSetChangeFact(false)
-    }, 2000)
-  }, [fact])
-
-  const isImageLoading = catImageSrc === null || isChangeFact
+  const isImageLoading = catImageUrl === null
 
   return (
     <>
@@ -53,11 +25,11 @@ export default function App () {
             isImageLoading
               ? <span className='loader' />
               : <picture className='fact__cat-picture'>
-                <img src={catImageSrc} alt={`Cat image says ${fact}`} className='fact__cat-img' />
+                <img src={catImageUrl} alt={`Cat image says ${fact}`} className='fact__cat-img' />
               </picture>
           }
         </div>
-        <button className='fact__button' onClick={() => { isSetChangeFact(true) }}>Change cat-fact</button>
+        <button className='fact__button' onClick={handleClick}>Change cat-fact</button>
       </main>
     </>
   )
